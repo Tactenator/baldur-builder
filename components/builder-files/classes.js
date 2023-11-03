e00b85d0495c9b4040d5970a470600fd15c02944
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-11-03 12:38:24 -0500

    classes works

--- components/builder-files/classes.js
+++ components/builder-files/classes.js
@@ -25,29 +25,29 @@ const Classes = () => {
         <div className="text-center text-3xl text-gold font-bold pb-10 ">Choose your Class</div>
         <div className=''>{ playerClass }</div>
         <div className="grid grid-cols-4 gap-10">
-            <button onClick={((e) => setPlayerClass('Elf'))} value="Barbarian" ><Image 
+            <button onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Barbarian']))} value="Barbarian" ><Image 
             className='hover:opacity-50 hover:cursor-pointer' src={Barbarian} alt='Barbarian' width={100} height={100}></Image></button>
-            <Image onClick={((e) => setPlayerClass('Bard'))} value="Bard" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Bard']))} value="Bard" className='hover:opacity-50 hover:cursor-pointer' 
             src={Bard} alt='Bard' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Cleric'))} value="Cleric" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Cleric']))} value="Cleric" className='hover:opacity-50 hover:cursor-pointer' 
             src={Cleric} alt='Cleric' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Druid'))} value="Druid" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Druid']))} value="Druid" className='hover:opacity-50 hover:cursor-pointer' 
             src={Druid} alt='Druid' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Fighter'))} value="Fighter" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Fighter']))} value="Fighter" className='hover:opacity-50 hover:cursor-pointer' 
             src={Fighter} alt='Fighter' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Monk'))} value="Monk" className='hover:opacity-50 hover:cursor-pointer'
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Monk']))} value="Monk" className='hover:opacity-50 hover:cursor-pointer'
             src={Monk} alt='Monk' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Paladin'))} value="Paladin" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Paladin']))} value="Paladin" className='hover:opacity-50 hover:cursor-pointer' 
             src={Paladin} alt='Paladin' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Ranger'))} value="Ranger" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Ranger']))} value="Ranger" className='hover:opacity-50 hover:cursor-pointer' 
             src={Ranger} alt='Ranger' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Rogue'))} value="Rogue" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Rogue']))} value="Rogue" className='hover:opacity-50 hover:cursor-pointer' 
             src={Rogue} alt='Rogue' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Sorcerer'))} value="Sorcerer" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Sorcerer']))} value="Sorcerer" className='hover:opacity-50 hover:cursor-pointer' 
             src={Sorcerer} alt='Sorcerer' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Warlock'))} value="Warlock" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Warlock']))} value="Warlock" className='hover:opacity-50 hover:cursor-pointer' 
             src={Warlock} alt='Warlock' width={100} height={100}></Image>
-            <Image onClick={((e) => setPlayerClass('Wizard'))} value="Wizard" className='hover:opacity-50 hover:cursor-pointer' 
+            <Image onClick={((e) => setPlayerClass(newClass => [...playerClass, 'Wizard']))} value="Wizard" className='hover:opacity-50 hover:cursor-pointer' 
             src={Wizard} alt='Wizard' width={100} height={100}></Image>
         </div>
     </div>
