e04bd11ab9ef78362364371f6e4784b4e1db18a6
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-11-04 12:19:34 -0500

    subrace page works; need to add buttons

--- app/character-creation/page.js
+++ app/character-creation/page.js
@@ -2,6 +2,7 @@
 
 import Classes from "@/components/builder-files/classes";
 import Races from "@/components/builder-files/races";
+import SubRaces from "@/components/builder-files/subrace";
 import { useState } from "react";
 
 export default function CharacterCreation() {
@@ -12,6 +13,7 @@ export default function CharacterCreation() {
         <div className="h-[100vh] bg-[url('../public/images/character-creation.webp')] bg-cover bg-no-repeat bg-center p-10">
             <Races></Races>
             <Classes></Classes>
+            <SubRaces></SubRaces>
         </div>
     );
 }
--- components/builder-files/subrace.js
+++ components/builder-files/subrace.js
@@ -2,38 +2,80 @@
 import { useGlobalContext } from '@/app/Context'
 
 
-const Races = () => {
+const SubRaces = () => {
 
     const { race, subrace, setSubrace } = useGlobalContext()
-    return ( 
-        <div className="bg-hero p-10 w-2/5 border-8 border-gold">
+
+    return (
+        <div>
+            {race === 'Elf' || race === 'Dwarf' || race === 'Drow' || race === 'Tiefling' || race === 'Halfling' || race === "Half-Elf" || race === "Gnome" || race === "Dragonborn" &&
+            <div className="bg-hero p-10 w-2/5 border-8 border-gold">
             <div className="text-center text-3xl text-gold font-bold pb-10 ">Choose your Race</div>
-            <div className="grid grid-cols-4 gap-10">
-                {/* <button onClick={((e) => setRace('Elf'))} value="Elf" ><Image value="Elf"
-                className='hover:opacity-50 hover:cursor-pointer' src={Elf} alt='Elf' width={100} height={100}></Image></button>
-                <Image onClick={((e) => setRace('Tiefling'))} value="Tiefling" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Tiefling} alt='Tiefling' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Drow'))} value="Drow" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Drow} alt='Drow' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Human'))} value="Human" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Human} alt='Human' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Githyanki'))} value="Githyanki" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Githyanki} alt='Githyanki' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Dwarf'))} value="Dwarf" className='hover:opacity-50 hover:cursor-pointer'
-                src={Dwarf} alt='Dwarf' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Half-Elf'))} value="Half-Elf" className='hover:opacity-50 hover:cursor-pointer' 
-                src={HalfElf} alt='Half-Elf' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Halfling'))} value="Halfling" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Halfling} alt='Halfling' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Gnome'))} value="Gnome" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Gnome} alt='Gnome' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Dragonborn'))} value="Dragonborn" className='hover:opacity-50 hover:cursor-pointer' 
-                src={DragonBorn} alt='Dragonborn' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Half-Orc'))} value="Half-Orc" className='hover:opacity-50 hover:cursor-pointer' 
-                src={HalfOrc} alt='Half-Orc' width={100} height={100}></Image> */}
+            { race === 'Elf' && 
+            <div className="grid grid-cols-2 gap-10">
+                <div>High Elf</div>
+                <div>Wood Elf</div>
             </div>
+            }
+            { race === 'Dwarf' && 
+            <div className="grid grid-cols-3 gap-10">
+                <div>Hill Dwarf</div>
+                <div>Mountain Dwarf</div>
+                <div>Duergar</div>
+            </div>
+            }
+            { race === 'Drow' && 
+            <div className="grid grid-cols-2 gap-10">
+                <div>Seladrine Drow</div>
+                <div>Lolth Drow</div>
+            </div>
+            }{ race === 'Tiefling' && 
+            <div className="grid grid-cols-3 gap-10">
+                <div>Asmodeus Tiefling</div>
+                <div>Mephistopheles Tiefling</div>
+                <div>Zariel Tiefling</div>
+            </div>
+            }
+            { race === 'Halfling' && 
+            <div className="grid grid-cols-2 gap-10">
+                <div>Lightfoot Halfling</div>
+                <div>Strongheart Halfling</div>
+            </div>
+            }
+            { race === 'Half-Elf' && 
+            <div className="grid grid-cols-3 gap-10">
+                <div>Wood Half-Elf</div>
+                <div>High Half-Elf</div>
+                <div>Drow Half-Elf</div>
+            </div>
+            }
+            { race === 'Gnome' && 
+            <div className="grid grid-cols-3 gap-10">
+                <div>Rock Gnome</div>
+                <div>Deep Gnome</div>
+                <div>Forest Gnome</div>
+            </div>
+            }
+            { race === 'Dragonborn' && 
+            <div className="grid grid-cols-3 gap-10">
+                <div>Black Dragonborn</div>
+                <div>Blue Dragonborn</div>
+                <div>Copper Dragonborn</div>
+                <div>Gold Dragonborn</div>
+                <div>Green Dragonborn</div>
+                <div>Red Dragonborn</div>
+                <div>Brass Dragonborn</div>
+                <div>Bronze Dragonborn</div>
+                <div>Silver Dragonborn</div>
+                <div>White Dragonborn</div>
+            </div>
+            }
         </div>
+            }
+            
+        </div>
+        
      );
 }
  
-export default Races;
+export default SubRaces;
