7533fff5dddc1bc63749b25d2bda6aa6be375e34
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-12-21 13:05:16 -0600

    fixing styles on race component

--- components/builder-files/races.js
+++ components/builder-files/races.js
@@ -26,28 +26,62 @@ const Races = () => {
             <div className="text-center text-3xl text-gold font-bold pb-10 ">Choose your Race</div>
             { race }
             <div className="grid grid-cols-4 gap-10">
-                <button onClick={((e) => setRace('Elf'))} value="Elf" ><Image value="Elf"
-                className='hover:opacity-50 hover:cursor-pointer' src={Elf} alt='Elf' width={100} height={100}></Image></button>
-                <Image onClick={((e) => setRace('Tiefling'))} value="Tiefling" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Tiefling} alt='Tiefling' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Drow'))} value="Drow" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Drow} alt='Drow' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Human'))} value="Human" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Human} alt='Human' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Githyanki'))} value="Githyanki" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Githyanki} alt='Githyanki' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Dwarf'))} value="Dwarf" className='hover:opacity-50 hover:cursor-pointer'
-                src={Dwarf} alt='Dwarf' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Half-Elf'))} value="Half-Elf" className='hover:opacity-50 hover:cursor-pointer' 
-                src={HalfElf} alt='Half-Elf' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Halfling'))} value="Halfling" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Halfling} alt='Halfling' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Gnome'))} value="Gnome" className='hover:opacity-50 hover:cursor-pointer' 
-                src={Gnome} alt='Gnome' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Dragonborn'))} value="Dragonborn" className='hover:opacity-50 hover:cursor-pointer' 
-                src={DragonBorn} alt='Dragonborn' width={100} height={100}></Image>
-                <Image onClick={((e) => setRace('Half-Orc'))} value="Half-Orc" className='hover:opacity-50 hover:cursor-pointer' 
-                src={HalfOrc} alt='Half-Orc' width={100} height={100}></Image>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Elf'))} value="Elf"
+                    className='hover:opacity-50 hover:cursor-pointer' src={Elf} alt='Elf' width={100} height={100} />
+                    <p className='text-gold'>Elf</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Tiefling'))} value="Tiefling" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={Tiefling} alt='Tiefling' width={100} height={100}></Image>
+                    <p className='text-gold'>Tiefling</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Drow'))} value="Drow" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={Drow} alt='Drow' width={100} height={100}></Image>
+                    <p className='text-gold'>Drow</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Human'))} value="Human" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={Human} alt='Human' width={100} height={100}></Image>
+                    <p className='text-gold'>Human</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Githyanki'))} value="Githyanki" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={Githyanki} alt='Githyanki' width={100} height={100}></Image>
+                    <p className='text-gold'>Githyanki</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Dwarf'))} value="Dwarf" className='hover:opacity-50 hover:cursor-pointer'
+                    src={Dwarf} alt='Dwarf' width={100} height={100}></Image>
+                    <p className='text-gold'>Dwarf</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Half-Elf'))} value="Half-Elf" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={HalfElf} alt='Half-Elf' width={100} height={100}></Image>
+                    <p className='text-gold'>Half-Elf</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Halfling'))} value="Halfling" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={Halfling} alt='Halfling' width={100} height={100}></Image>
+                    <p className='text-gold'>Halfling</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Gnome'))} value="Gnome" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={Gnome} alt='Gnome' width={100} height={100}></Image>
+                    <p className='text-gold'>Gnome</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Dragonborn'))} value="Dragonborn" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={DragonBorn} alt='Dragonborn' width={100} height={100}></Image>
+                    <p className='text-gold'>Dragonborn</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <Image onClick={((e) => setRace('Half-Orc'))} value="Half-Orc" className='hover:opacity-50 hover:cursor-pointer' 
+                    src={HalfOrc} alt='Half-Orc' width={100} height={100}></Image>
+                    <p className='text-gold'>Half-Orc</p>
+                </div>
+                
             </div>
         </div>
      );
